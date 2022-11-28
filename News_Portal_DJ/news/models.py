8860feebfe71d240a8c51730c2867764bf7ada5f
Vladimir <imeno-tot-vova@mail.ru>
2022-11-28 15:28:05 +0300

    A model of a news portal has been created

--- News_Portal_DJ/news/models.py
+++ News_Portal_DJ/news/models.py
@@ -2,35 +2,38 @@ from django.db import models
 from django.contrib.auth.models import User
 from .resources import *
 
+
 class Author(models.Model):
-    id_users = models.OneToOneField(User, on_delete=models.CASCADE)
-    rating_users = models.IntegerField(default = 0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating_author = models.IntegerField(default=0)
+
     def update_rating(self):
-        self.rating_users = 0
-        for posts in Post.objects.all():
-            if posts.id_authors == self.id:
-                self.rating_users += posts.rating_post * 3
-            for comments_to_post in Comment.objects.filter(id_post = posts.id):
-                if posts.id_authors == self.id:
-                    self.rating_users += comments_to_post.rating_post
+        self.rating_author = 0
+        for post_ in Post.objects.all():
+            if post_.author_id == self.id:
+                self.rating_author += post_.rating_post * 3
+            for comments_to_post in Comment.objects.filter(post=post_.id):
+                if comments_to_post.user_id == self.id:
+                    self.rating_author += comments_to_post.rating_comment
         for comment_author in Comment.objects.all():
-            if Comment.id_users == self.id:
-                self.rating_users += comment_author.raiting_comment
+            if comment_author.user_id == self.id:
+                self.rating_author += comment_author.rating_comment
         self.save()
 
-class Category(models.Model):
 
-    name_category = models.CharField(unique=True, max_length = 2, choices = category)
+class Category(models.Model):
+    name_category = models.CharField(unique=True, max_length=2,\
+                    choices=category)
+
 
 class Post(models.Model):
-    id_authors = models.ForeignKey(Author, on_delete=models.CASCADE)
-    type_of_post = models.CharField(max_length = 2, choices = type_post)
-    time_in = models.DateTimeField(auto_now_add = True)
-    title = models.CharField(max_length = 255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    type_of_post = models.CharField(max_length=2, choices=type_post)
+    time_in = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
     text = models.TextField()
-    rating_post = models.IntegerField(default = 0)
-
-    post_category = models.ManyToManyField(Category, through = 'PostCategory')
+    rating_post = models.IntegerField(default=0)
+    post_category = models.ManyToManyField(Category, through='PostCategory')
 
     @property
     def rating(self):
@@ -38,7 +41,8 @@ class Post(models.Model):
 
     @rating.setter
     def rating(self, value):
-        self.rating_post = value if value >= 0 and isinstance(value, int) else 0
+        self.rating_post = value if value >= 0 and isinstance(value, int) \
+            else 0
         self.save()
 
     def like(self):
@@ -49,19 +53,22 @@ class Post(models.Model):
         self.rating_post -= 1
         self.save()
 
+    @property
     def preview(self):
         return self.text[:124] + '...'
 
+
 class PostCategory(models.Model):
-    id_post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    id_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
 
 class Comment(models.Model):
-    id_users = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     text_comment = models.TextField()
-    time_in = models.DateTimeField(auto_now_add = True)
-    rating_comment = models.IntegerField(default = 0)
+    time_in = models.DateTimeField(auto_now_add=True)
+    rating_comment = models.IntegerField(default=0)
 
     @property
     def rating(self):
@@ -69,7 +76,8 @@ class Comment(models.Model):
 
     @rating.setter
     def rating(self, value):
-        self.rating_comment = value if value >= 0 and isinstance(value, int) else 0
+        self.rating_comment = value if value >= 0 and isinstance(value, int) \
+            else 0
         self.save()
 
     def like(self):
@@ -79,5 +87,3 @@ class Comment(models.Model):
     def dislike(self):
         self.rating_comment -= 1
         self.save()
-
-
