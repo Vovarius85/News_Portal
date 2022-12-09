03398295568190cf758f9074bdac4bbd455216f3
Vladimir <imeno-tot-vova@mail.ru>
2022-12-09 18:01:12 +0300

    Views and templates are made

--- News_Portal_DJ/news/models.py
+++ News_Portal_DJ/news/models.py
@@ -57,6 +57,9 @@ class Post(models.Model):
     def preview(self):
         return self.text[:124] + '...'
 
+    def __str__(self):
+        return f'{self.title}{self.rating_post}{self.author}'
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
